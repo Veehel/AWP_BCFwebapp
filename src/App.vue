36f8869716767e865a917be810c33c3392dc11f9
Jblegoff <jlegoff@et.esiea.fr>
2019-09-24 15:58:36 +0200

    added card vue

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,27 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>bcf</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Bcf/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Bcf from './components/Bcf.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Bcf
   },
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
---
+++ src/components/Bcf.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      :flat="flat"
+      :loading="loading"
+      :outlined="outlined"
+      :elevation="elevation"
+      :raised="raised"
+      :width="width"
+      :height="height"
+    >
+      <v-img
+        v-if="media"
+        class="white--text"
+        height="1000px"
+        src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/70504862_783029895486614_6712234972486303744_n.jpg?_nc_cat=105&_nc_oc=AQnprZSpljtXbhIAyjuTQK7snEEtF2HFM_3wf3gJ_WX9YCf41b01zJdUU_9zkzxdK88&_nc_ht=scontent-cdg2-1.xx&oh=f0d457c154850e14133208bcf7575955&oe=5E01C626"
+      >
+        <v-card-title class="align-end fill-height">I'm a title</v-card-title>
+      </v-img>
+      <v-card-title v-else>I'm a title</v-card-title>
+
+      <v-card-text>I'm card text</v-card-text>
+      <v-card-actions v-if="actions">
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn text>Click</v-btn>
+        <v-btn outlined>Click</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<style scoped>
+  .v-input__slider {
+    width: 100%;
+  }
+</style>
+
+<script>
+export default {
+  data: () => ({
+    flat: false,
+    media: true,
+    loading: false,
+    actions: true,
+    outlined: false,
+    elevation: undefined,
+    raised: false,
+    width: 344,
+    height: undefined
+  })
+}
+</script>
