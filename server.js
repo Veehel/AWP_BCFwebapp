9139761d4121bda055d8387398c5bb260ac20c2b
unknown <theodoredroissart@yahoo.fr>
2019-09-24 17:49:33 +0200

    commit login page and link to server

---
+++ server.js
@@ -0,0 +1,131 @@
+/**
+ *
+ * entrez la commande suivante:
+ * npm install --save express express-session body-parser morgan cors
+ * puis node server.js
+ * exemple complet à l'adresse https://github.com/Musinux/first-vue-app
+ */
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const session = require('express-session')
+const path = require('path')
+
+const app = express()
+
+app.use(session({
+  secret: 'BCFBCF', // changez cette valeur
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // ne changez que si vous avez activé le https
+}))
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname)))
+
+const users = [{
+  username: 'admin',
+  password: 'admin'
+},
+{
+  username: 'zatuar',
+  password: 'wxcvbn'
+}]
+
+app.get('/api/test', (req, res) => {
+  console.log('ce console.log est appelé au bon moment')
+  res.json([
+    {
+      title: 'truc',
+      content: 'machin'
+    }, {
+      title: 'truc2',
+      content: 'machin2'
+    }
+  ])
+})
+
+app.post('/api/login', (req, res) => {
+  console.log('req.body', req.body)
+  console.log('req.query', req.query)
+  if (!req.session.userId) {
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+    console.log('user', user)
+    if (!user) {
+      // gérez le cas où on n'a pas trouvé d'utilisateur correspondant
+      res.json({
+        message: 'Erreur pseudo ou mot de passe'
+      })
+    } else {
+      // connect the user
+      req.session.userId = 1000 // connect the user, and change the id
+      res.json({
+        message: 'Connected'
+      })
+    }
+  } else {
+    res.status(401)
+    res.json({
+      message: 'you are already connected'
+    })
+  }
+})
+
+app.get('/api/logout', (req, res) => {
+  if (!req.session.userId) {
+    res.status(401)
+    res.json({
+      message: 'you are already disconnected'
+    })
+  } else {
+    req.session.userId = 0
+    res.json({
+      message: 'you are now disconnected'
+    })
+  }
+})
+
+app.post('/api/signup', (req, res) => {
+  console.log('req.body', req.body)
+  console.log('req.query', req.query)
+  if (!req.session.userId) {
+    const user = users.find(u => u.username === req.body.username)
+    console.log(user)
+    if (!user) {
+      // push dans notre tableau users la création du membre
+      console.log('OK')
+      res.json({
+        message: 'Création du compte réussi'
+      })
+    } else {
+      // connect the user
+      console.log('KO')
+      res.json({
+        message: 'Pseudo déjà utilisé'
+      })
+    }
+  } else {
+    res.status(401)
+    res.json({
+      message: 'you are already connected'
+    })
+  }
+})
+app.get('/api/admin', (req, res) => {
+  if (!req.session.userId || req.session.isAdmin === false) {
+    res.status(401)
+    res.json({ message: 'Unauthorized' })
+    return
+  }
+
+  res.json({
+    message: 'congrats, you are connected'
+  })
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
---
+++ src/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12" md="5">
+              <v-text-field v-model="pseudo" label="pseudo" required></v-text-field>
+            </v-col>
+            <v-col cols="12" md="5">
+          <v-text-field type="password" v-model="password" label="password" required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+          <v-col cols="12" md="8">
+          <v-btn v-on:click="login"> Log in </v-btn>
+          <v-btn v-on:click="signup"> Sign up </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  data: () => ({
+    pseudo: '',
+    password: ''
+  }),
+  methods: {
+    async login () {
+      const jsondata = await this.axios.post('http://localhost:4000/api/login', {
+        username: this.pseudo,
+        password: this.password
+      })
+      console.log(jsondata.data)
+      this.pseudo = ''
+      this.password = ''
+    },
+    async signup () {
+      const jsondata = await this.axios.post('http://localhost:4000/api/signup', {
+        username: this.pseudo,
+        password: this.password
+      })
+      console.log(jsondata.data)
+      this.pseudo = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
