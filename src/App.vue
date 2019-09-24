752c4a4bcdf110f108f68192a0a92bdb9c79a8c8
Jblegoff <jlegoff@et.esiea.fr>
2019-09-24 17:48:26 +0200

    add sign in/sign up buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>BCF</span>
+        <span class="font-weight-light"> ~ Your tastiest memes</span>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn
+        text
+        target="_blank"
+      >
+        <span class="mr-2">sign in</span>
+      </v-btn>
+      <v-btn
+        text
+        target="_blank"
+      >
+        <span class="mr-2">sign up</span>
+      </v-btn>
+    </v-app-bar>
+
     <v-content>
       <Bcf/>
     </v-content>
