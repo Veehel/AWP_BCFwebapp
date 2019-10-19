d2066a0596890f1ecb7cc9be52fdc5b513bb58ed
Veehel <veeehel@gmail.com>
2019-10-19 17:00:26 +0200

    remove unwanted changes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <v-app>
     <v-app-bar app>
+      <head></head>
       <v-toolbar-title class="headline" dark collapse="true">
         <span>BCF ~ Your tastiest memes</span>
       </v-toolbar-title>
       <div class="flex-grow-1"></div>
       <v-toolbar-items>
         <v-btn>Sign In - Sign Up</v-btn>
-      </v-toolbar-items>
+      </v-toolbar-items
+      <v-toolbar-title class="headline text-uppercase">
+        <span>BCF</span>
+        <span class="font-weight-light"> ~ Your tastiest memes</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        target="./components/Login"
+      >
+        <span class="mr-2">sign in</span>
+      </v-btn>
+
+>>>>>>> e421d0222e46b9371dc4230f7609485d94a964f2
     </v-app-bar>
     <v-content>
-      <Bcf/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Bcf from './components/Bcf.vue'
-
 export default {
   name: 'App',
   components: {
-    Bcf
   },
   data: () => ({
     //
--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+<<<<<<< HEAD
+=======
+
+import Vuerouter from 'vue-router'
+import Bcf from './components/Bcf'
+import Login from './components/Login'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+>>>>>>> e421d0222e46b9371dc4230f7609485d94a964f2
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(Vuerouter)
+
+const routes = [
+  { path: '/Bcf', component: Bcf, meta: { requiresAuth: false }, name: 'mainpage' },
+  { path: '/', component: Login, meta: { requiresAuth: false } },
+  { path: 'Login', component: Login, meta: { requiresAuth: false }, name: 'login' }
+]
+
+const router = new Vuerouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
